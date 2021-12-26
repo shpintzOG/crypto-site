c5ce23349d811578d1d5734919e58412b8e7d27a
coolistkovbel@icloud.com <coolistkovbel@icloud.com>
2021-12-26 15:47:13 -0500

    banner and small paragraph

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Site in progress... wait for it
-        </p>
-        <a
-          className="App-link"
-          href="https://ghostieve.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn More
-        </a>
-      </header>
+
+    {/* section one */}
+    <section>
+
+      <img src="./pics/banner.jpg" alt='what is life' />
+
+    </section>
+
+    <div>
+      <p>
+        Alright well this is going to be something I would like to share with my friends and family. <strong>Crypto.com</strong> is going to be one of the biggest places where most people in america are going to be buying and selling crypto currency. I have been using crypto.com for about two years now. And would like to try and help out and create a community where if you are new to crypto, you wont feel lost. Click on the <a href='https://crypto.com/app/y8yjabr8my' target="_blank" rel='noreferrer'>links</a> below to get the app and create an account. Use my refferal code <code>y8yjabr8my</code> so we both can get $25 to start off this journey. And be sure to get your hands on the ruby red card. If you want to know more about it check out my youtube channel. Join the community. <strong>Fortune favours the brave </strong>
+      </p>
+    </div>
+
+
+
+
+
     </div>
   );
 }
