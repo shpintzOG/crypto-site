09f589adc3b661d73b437104fb8b6cf5bd5129ce
coolistkovbel@icloud.com <coolistkovbel@icloud.com>
2021-12-26 13:38:51 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site in progress... wait for it
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ghostieve.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
