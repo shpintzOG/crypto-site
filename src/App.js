c880ae7ce380fecb24e7517c8306ac0172f4dc0c
coolistkovbel@icloud.com <coolistkovbel@icloud.com>
2021-12-29 19:37:49 -0500

    finish css soon adding axios maybe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-
+import banner from './pics/banner.jpg'
+import cryptoLogo from './pics/crypto-com.png'
+import cryptoDido from './pics/crypto-dido.jpg'
 import './App.css';
 
 function App() {
@@ -6,20 +8,58 @@ function App() {
     <div className="App">
 
     {/* section one */}
-    <section>
+    <section className='banner'>
 
-      <img src="./pics/banner.jpg" alt='what is life' />
+      <img src={banner} alt='what is life' />
 
     </section>
 
-    <div>
+    <div className='description'>
+      <h2>Getting Started</h2>
       <p>
-        Alright well this is going to be something I would like to share with my friends and family. <strong>Crypto.com</strong> is going to be one of the biggest places where most people in america are going to be buying and selling crypto currency. I have been using crypto.com for about two years now. And would like to try and help out and create a community where if you are new to crypto, you wont feel lost. Click on the <a href='https://crypto.com/app/y8yjabr8my' target="_blank" rel='noreferrer'>links</a> below to get the app and create an account. Use my refferal code <code>y8yjabr8my</code> so we both can get $25 to start off this journey. And be sure to get your hands on the ruby red card. If you want to know more about it check out my youtube channel. Join the community. <strong>Fortune favours the brave </strong>
+        Alright well this is going to be something I would like to share with my friends and family. <strong>Crypto.com</strong> is going to be one of the biggest places where most people in america are going to be buying and selling crypto currency. I have been using crypto.com for about two years now. And would like to try and help out and create a community where if you are new to crypto, you wont feel lost. Click on the <a href='https://crypto.com/app/y8yjabr8my' target="_blank" rel='noreferrer'>links</a> below to get the app and create an account. Use my refferal code <code>y8yjabr8my</code> so we both can get $25 to start off this journey. And be sure to get your hands on the ruby red card. If you want to know more about it check out my youtube channel. Join the community.<br /> <strong>Fortune favours the brave </strong>
       </p>
     </div>
 
+    <div className='links'>
+
+      <div className='crypto-com'>
+        <h3 className='linkTitle'>Crypto.com</h3>
+        <div className='image-container'>
+          <img src={cryptoLogo} alt="crypto.com" />
+        </div>
+        <a className='linkLink' target="_blank" href='https://crypto.com/app/y8yjabr8my' rel='noreferrer' >
+          crypto link
+        </a>
+      </div>
+
+      <div className='crypto-dido'>
+        <h3 className='linkTitle'>Youtube Channel</h3>
+        <div className='image-container'>
+          <img src={cryptoDido} alt='crypto dido' />
+        </div>
+        <a className='linkLink' href='https://www.youtube.com/channel/UCfgNvLLaiB3v96h8nQ4jFsA' target="_blank" rel='noreferrer'>
+          Youtube link
+        </a>
+      </div>
+
+    </div>
 
 
+    <div className='crypto-socials'>
+      <h3>Join the community channels</h3>
+      <ul>
+        <li>
+          <a>Telegram</a>
+        </li>
+        <li>
+          <a>Discord</a>
+        </li>
+        <li>
+          <a>Reddit</a>
+        </li>
+      </ul>
+    </div>
 
 
     </div>
